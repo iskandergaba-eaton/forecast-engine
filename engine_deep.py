f87619259941a52fa6f92c7638f5acb452518b19
Gaba, Iskander <IskanderGaba@Eaton.com>
2022-03-23 16:15:21 +0100

    Fixed the deep learning integration with master

--- engine_deep.py
+++ engine_deep.py
@@ -1,20 +1,21 @@
 # Import librairies
+from util import SNR, prominence
+from keras.callbacks import ModelCheckpoint
+from keras.layers.core import Dense
+from keras.models import Model, Sequential, load_model
+from keras.layers import Input, LSTM, RepeatVector
+from sklearn.preprocessing import StandardScaler
+from scipy.stats import entropy
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import statsmodels.api as sm
 plt.style.use('fivethirtyeight')
 plt.rcParams['figure.figsize'] = 18, 8
-from scipy.stats import entropy
-from sklearn.preprocessing import StandardScaler
 
-from keras.layers import Input, LSTM, RepeatVector
-from keras.models import Model, Sequential, load_model
-from keras.layers.core import Dense
-from keras.callbacks import ModelCheckpoint
 
 np.random.seed(42)
-from util import SNR, prominence
+
 
 class EatonPowerForecastModel():
     """
@@ -29,7 +30,7 @@ class EatonPowerForecastModel():
     recurrent neural network
     - forecast_model: create LSTM model
     """
-    
+
     def __init__(self, window, autoencoder_activation, model_activation):
         """Initialize the algorithm, with a fixed rolling window
         :param window: (int)
@@ -37,7 +38,7 @@ class EatonPowerForecastModel():
         self.window = window
         self.autoencoder_activation = autoencoder_activation
         self.model_activation = model_activation
-        
+
     def _extract_time_series(self, x, i, size):
         """Function extracting the sub time series from a pandas Series. We use
         this function  because the pandas Series does not have same properties
@@ -49,38 +50,39 @@ class EatonPowerForecastModel():
         # Need to differentiate the last case
         if i != size:
             x_int = x[x.index[i-self.window]:x.index[i]]
-        else: # size = x.shape[0] (differentiate last sliding window )
+        else:  # size = x.shape[0] (differentiate last sliding window )
             x_int = x[x.index[i-self.window]:]
         return x_int
-        
+
     def ts_mean(self, x):
         """Computes the time series mean for a rolling window
         :param x: (pandas Series) time series
         """
         x.name = 'average'
         return x.rolling(self.window).mean()
-    
+
     def ts_var(self, x):
         """Computes the time series variance for a rolling window
         :param x: (pandas Series) time series
         """
         x.name = 'variance'
         return x.rolling(self.window).var()
-    
+
     def ts_autocorrelation(self, x):
         """Computes the time series autocorrelation for a rolling window
         :param x: (pandas Series) time series
         """
         # Initialize output
-        res = pd.Series(data = None, index = x.index, name = 'autocorr')
+        res = pd.Series(data=None, index=x.index, name='autocorr')
         # Loop through the time series
         for i in range(self.window, x.shape[0] + 1):
-            #by default n_lags = 1
-            res[res.index[i-1]] = self._extract_time_series(x, i, x.shape[0]).autocorr()       
+            # by default n_lags = 1
+            res[res.index[i-1]
+                ] = self._extract_time_series(x, i, x.shape[0]).autocorr()
         return res
 
-    def ts_partial_autocorrelation(self ,x): 
-        # TODO 
+    def ts_partial_autocorrelation(self, x):
+        # TODO
         return 0
 
     def ts_entropy(self, x):
@@ -88,21 +90,22 @@ class EatonPowerForecastModel():
         :param x: (pandas Series) time series
         """
         # Initialize output
-        res = pd.Series(data = None, index = x.index, name = 'entropy')
+        res = pd.Series(data=None, index=x.index, name='entropy')
         # Loop through the time series
         for i in range(self.window, x.shape[0] + 1):
-            res[res.index[i-1]] = entropy(self._extract_time_series(x, i, x.shape[0])) 
+            res[res.index[i-1]
+                ] = entropy(self._extract_time_series(x, i, x.shape[0]))
         return res
-    
+
     def ts_trend(self, x):
         """Computes the trend coefficient and the variance of the residuals of
         the time series versus the trend, for a rolling window
         :param x: (pandas Series) time series
         """
         # Initialize outputs
-        res1 = pd.Series(data = None, index = x.index, name = 'trend')
-        res2 = pd.Series(data = None, index = x.index, name = 'trend_p_value')
-        res3 = pd.Series(data = None, index = x.index, name = 'residuals_variance')
+        res1 = pd.Series(data=None, index=x.index, name='trend')
+        res2 = pd.Series(data=None, index=x.index, name='trend_p_value')
+        res3 = pd.Series(data=None, index=x.index, name='residuals_variance')
         # Create the X, to compute the trend
         x_int = np.arange(self.window)
         x_int = sm.add_constant(x_int)
@@ -118,48 +121,48 @@ class EatonPowerForecastModel():
             res2[res2.index[i-1]] = results.pvalues['x1']
             # Strength of trend
             res3[res3.index[i-1]] = np.var(results.predict(x_int) - y_int)
-            
+
         return res1, res2, res3
-    
+
     def ts_spike(self, x):
         """Computes the spike of a time series, for a rolling window
         :param x: (pandas Series) time series
         spike : high values (like peaks) from the std 
         """
         # Initialize output
-        res = pd.Series(data = None, index = x.index, name = 'spike')
+        res = pd.Series(data=None, index=x.index, name='spike')
         # Loop through the time series
         for i in range(self.window, x.shape[0] + 1):
             # Extract the corresponding time series window and computes the spike
             x_int = self._extract_time_series(x, i, x.shape[0])
             res[res.index[i-1]] = self.function_spike(x_int)
         return res
-    
+
     def function_spike(self, x):
         """Computes the spike function for a time series
         :param x: (pandas Series) time series
         """
-        
+
         # Computes the max and min spikes versus the average
         M, m = x.max() - x.mean(), x.min() - x.mean()
-        # Return the biggest spike    
+        # Return the biggest spike
         return M if np.abs(M) > np.abs(m) else m
-    
+
     def ts_crossing_points(self, x):
         """Computes the number of times the mean is crossed by the time series 
         for a rolling window
         :param x: (pandas Series) time series
         """
-        
+
         # Initialize output
-        res = pd.Series(data = None, index = x.index, name = 'crossing_points')
+        res = pd.Series(data=None, index=x.index, name='crossing_points')
         # Loop through time series
         for i in range(self.window, x.shape[0] + 1):
             # Extract the corresponding time series window and computes the crossing points
             x_int = self._extract_time_series(x, i, x.shape[0])
             res[res.index[i-1]] = self.function_crossing_points(x_int)
         return res
-            
+
     def function_crossing_points(self, x):
         """Computes the number of times the times series cross its mean
         :param x: (pandas Series) time series
@@ -170,24 +173,24 @@ class EatonPowerForecastModel():
         x = x.diff().fillna(0)
         return np.abs(x).sum() / 2
 
-    def ts_SNR(self, x): 
-        snr = SNR(x, peaks = {k : x[k] for k in x.index}, w_size = 10).values()
-        res = pd.Series(snr, index = x.index , name = 'SNR')
+    def ts_SNR(self, x):
+        snr = SNR(x, peaks={k: x[k] for k in x.index}, w_size=10).values()
+        res = pd.Series(snr, index=x.index, name='SNR')
         res = res.astype(np.float64)
         return res
-                                
-    def ts_prominence(self, x): 
-        prominences = prominence(x, peaks = {k : x[k] for k in x.index}).values()
-        res = pd.Series(prominences, index = x.index , name = 'prominence')
+
+    def ts_prominence(self, x):
+        prominences = prominence(x, peaks={k: x[k] for k in x.index}).values()
+        res = pd.Series(prominences, index=x.index, name='prominence')
         res = res.astype(np.float64)
-        return res 
+        return res
 
     def compute_time_series_features(self, x):
         """Computes the different features of the time series
         :param x: (pandas Series) time series
         """
         # In case, transform the initial input into a pandas Series
-        x = pd.Series(x, name = 'time_series')
+        x = pd.Series(x, name='time_series')
         # Computes the individual features
         x1 = self.ts_mean(x)
         x2 = self.ts_var(x)
@@ -199,24 +202,24 @@ class EatonPowerForecastModel():
         x10 = self.ts_SNR(x)
         x11 = self.ts_prominence(x)
         # Concatenate them into a DataFrame
-        res = pd.concat([x1,x2,x3,x4,x8,x9, x10, x11], axis = 1)
+        res = pd.concat([x1, x2, x3, x4, x8, x9, x10, x11], axis=1)
         # Look also at the features changes
         res_diff = res.diff()
         res_diff.columns = [x + '_diff' for x in res_diff.columns]
-        return pd.concat([res, res_diff], axis = 1)
-    
+        return pd.concat([res, res_diff], axis=1)
+
     def initialize_dataset(self, y):
         """Build the dataset based on a time series feature y
         :param x: (pandas Series) time series
         """
         # Compute all variables
         X = self.compute_time_series_features(y)
-        X = X.iloc[self.window-1:,:]
+        X = X.iloc[self.window-1:, :]
         y = y.iloc[self.window-1:]
         X = X.fillna(method='ffill').fillna(X.mean())
         return X, y
-        
-    def sequential_autoencoder(self, x, num_layers, timesteps = 5):
+
+    def sequential_autoencoder(self, x, num_layers, timesteps=5):
         """LSTM Autoencoder to extract high level features
         Note that the x input is still in a raw format [m,n] where m are dates 
         and n are the features. The sequence length is still given by timesteps,
@@ -226,38 +229,42 @@ class EatonPowerForecastModel():
         :param num_layers: (int) number of layers
         :param timesteps: (int) depth of time series for the LSTM cell
         """
-        
+
         # Input
-        inputs = Input(shape = (timesteps, x.shape[-1]))
-        
+        inputs = Input(shape=(timesteps, x.shape[-1]))
+
         # Encoder
-        encoder = LSTM(num_layers, activation = self.autoencoder_activation)(inputs)
-        
+        encoder = LSTM(
+            num_layers, activation=self.autoencoder_activation)(inputs)
+
         # Decoder
         decoder = RepeatVector(timesteps)(encoder)
-        decoder = LSTM(x.shape[-1], return_sequences = True, activation = self.autoencoder_activation)(decoder)
-        
+        decoder = LSTM(x.shape[-1], return_sequences=True,
+                       activation=self.autoencoder_activation)(decoder)
+
         # Models
         sequential_autoencoder = Model(inputs, decoder)
-        sequential_autoencoder.compile(loss = 'mean_squared_error', optimizer = 'adam')
+        sequential_autoencoder.compile(
+            loss='mean_squared_error', optimizer='adam')
         encoder = Model(inputs, encoder)
-        encoder.compile(loss = 'mean_squared_error', optimizer = 'adam')
-        
+        encoder.compile(loss='mean_squared_error', optimizer='adam')
+
         return sequential_autoencoder, encoder
-    
+
     def prepare_data_autoencoder(self, X, y):
         """Prepare the data overall, by scaling
         :param X: (pandas DataFrame) features
         :param y: (pandas Series) time series
         """
-        
+
         # Initialize the two scalers
         sc_x, sc_y = StandardScaler(), StandardScaler()
         # Convert data into proper matrix for scaling, and transform them
-        X_values, y_values = X.values, y.values.reshape(-1,1)
-        X_values, y_values = sc_x.fit_transform(X_values), sc_y.fit_transform(y_values)
+        X_values, y_values = X.values, y.values.reshape(-1, 1)
+        X_values, y_values = sc_x.fit_transform(
+            X_values), sc_y.fit_transform(y_values)
         return X_values, y_values, sc_x, sc_y
-        
+
     def generate_encoder(self, X, num_layers, timestep, epochs=200):
         """Generate and fit the autoencoder. It returns the model with the best
         validation metrics. The compression dimension is num_layers.
@@ -266,21 +273,22 @@ class EatonPowerForecastModel():
         :param timestep: (int)
         :param: epochs: (int)
         """
-        checkpoint = ModelCheckpoint('autoencoder.h5', 
-                                     monitor = 'val_loss', 
-                                     verbose=1, 
-                                     save_best_only=True, 
+        checkpoint = ModelCheckpoint('autoencoder.h5',
+                                     monitor='val_loss',
+                                     verbose=1,
+                                     save_best_only=True,
                                      mode='min')
-        autoencoder, encoder = self.sequential_autoencoder(X, num_layers, timestep)
-        autoencoder.fit(X, X, batch_size=256, 
-                              epochs=epochs, 
-                              validation_split=0.2, 
-                              callbacks=[checkpoint])
-        
+        autoencoder, encoder = self.sequential_autoencoder(
+            X, num_layers, timestep)
+        autoencoder.fit(X, X, batch_size=256,
+                        epochs=epochs,
+                        validation_split=0.2,
+                        callbacks=[checkpoint])
+
         autoencoder = load_model('autoencoder.h5')
         # Return only the encoder part
         return Model(autoencoder.layers[0].input, autoencoder.layers[1].output)
-        
+
     def forecast_model(self, x, num_layers, timesteps):
         """LSTM model for forecasting. 
         Note that the x input is still in a raw format [m,n] where m are dates 
@@ -291,16 +299,16 @@ class EatonPowerForecastModel():
         :param num_layers: (int) number of layers
         :param timesteps: (int) depth of time series for the LSTM cell
         """
-        
+
         # LSTM Model
         model = Sequential()
-        model.add(LSTM(units = num_layers, input_shape = (timesteps, x.shape[-1]),
-                       activation = self.model_activation))
-        model.add(Dense(1, activation = None))
-        model.compile(loss = 'mean_squared_error', optimizer = 'adam')
-        
+        model.add(LSTM(units=num_layers, input_shape=(timesteps, x.shape[-1]),
+                       activation=self.model_activation))
+        model.add(Dense(1, activation=None))
+        model.compile(loss='mean_squared_error', optimizer='adam')
+
         return model
-    
+
     def create_variable_for_model(self, x, timesteps):
         """Transform the variables inputs into an appropriate format for the 
         second model. The x input needs to be already in a numpy format.
@@ -309,9 +317,9 @@ class EatonPowerForecastModel():
         """
         # Initialize output
         res = []
-        
+
         # Create the output
         for idx in range(x.shape[0] - timesteps):
-            res.append(x[idx:idx+timesteps,:])
-        
+            res.append(x[idx:idx+timesteps, :])
+
         return np.array(res)
--- main_deep.py
+++ main_deep.py
@@ -1,61 +1,67 @@
 # Import librairies
+from engine_deep import EatonPowerForecastModel
+from util import ungap
+from util import load_data
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import statsmodels.api as sm
 plt.style.use('fivethirtyeight')
 plt.rcParams['figure.figsize'] = 18, 8
 
 np.random.seed(42)
-from util import load_data
-from util import  add_noise
-from engine_deep import EatonPowerForecastModel
 
 if __name__ == "__main__":
-    # Global variables 
-    k,h,lag,threshold = (10, 1, 10, 1)
+    # Global variables
+    k, h, lag, threshold = (10, 1, 10, 1)
     influence = 1
     # Data loading
-    DCs = ["simtcsvc01", "usltccnvcenter4", "usltccnvcenter5", "usltcsvcenter2", "usltcsvcenter3"]
-    versions_gap = ["19-02-2022-gaps"]
+    DCs = ["simtcsvc01", "usltccnvcenter4",
+           "usltccnvcenter5", "usltcsvcenter2", "usltcsvcenter3"]
+    versions_gap = ["19-02-2022"]
     root_gap, dc_gap = '../data/clean/{}'.format(versions_gap[-1]), DCs[0]
     dfs_mean_gap, powers_mean_noisy = {}, {}
-    for dc in DCs :
+    for dc in DCs:
         dfs_mean_gap[dc] = load_data(root_gap, dc, agg_func=np.mean, freq='6H')
         print(dc, " loaded")
-        
-    for dc in DCs :
-        powers_mean_noisy[dc] = add_noise(dfs_mean_gap[dc], colname ='power')
-        print('noise added on ', dc)
-    
-    # Test on mean power consumption 
+
+    for dc in DCs:
+        powers_mean_noisy[dc] = ungap(dfs_mean_gap[dc], col_name='power')
+        print('Ungapped {}'.format(dc))
+
+    # Test on mean power consumption
     dc = DCs[1]
     time_window = 5
     autoencoder_activation = 'tanh'
     model_activation = 'tanh'
     n_layers_autoencoder = 200
     n_units = 5
-    ts_power = powers_mean_noisy[dc]       
-    y = pd.Series(list(ts_power), index = range(len(ts_power)))
+    ts_power = powers_mean_noisy[dc]
+    y = pd.Series(list(ts_power), index=range(len(ts_power)))
     # split train/test (80% ; 20%)
     split = 8*len(y)//10
     y_train, y_val = y[:split], y
 
-    alg = EatonPowerForecastModel(time_window, autoencoder_activation, model_activation)
+    alg = EatonPowerForecastModel(
+        time_window, autoencoder_activation, model_activation)
     Xtrain, ytrain = alg.initialize_dataset(y_train)
     Xval, yval = alg.initialize_dataset(y_val)
 
     # Scale down the data
-    xtrain_std, ytrain_std, sc_xtrain, sc_ytrain = alg.prepare_data_autoencoder(Xtrain, ytrain)
-    xval_std, yval_std, sc_xval, sc_yval = alg.prepare_data_autoencoder(Xval, yval)
+    xtrain_std, ytrain_std, sc_xtrain, sc_ytrain = alg.prepare_data_autoencoder(
+        Xtrain, ytrain)
+    xval_std, yval_std, sc_xval, sc_yval = alg.prepare_data_autoencoder(
+        Xval, yval)
 
-    X_train, Y_train = alg.create_variable_for_model(xtrain_std, time_window), ytrain_std[time_window:]
-    X_val, Y_val =  alg.create_variable_for_model(xval_std, time_window), yval_std[time_window:]
+    X_train, Y_train = alg.create_variable_for_model(
+        xtrain_std, time_window), ytrain_std[time_window:]
+    X_val, Y_val = alg.create_variable_for_model(
+        xval_std, time_window), yval_std[time_window:]
 
     # Dimensionality reduction
     encoder = alg.generate_encoder(X_train, n_layers_autoencoder, time_window)
     xx_train = encoder.predict(X_train)
-    xx_val  = encoder.predict(X_val)
+    xx_val = encoder.predict(X_val)
 
     x_train = alg.create_variable_for_model(xx_train, time_window)
     y_train = Y_train[time_window:]
@@ -70,9 +76,14 @@ if __name__ == "__main__":
 
     # Plotting
     train_id, val_id = range(len(y_train)), range(len(y_train), len(y_val))
-    plt.plot(train_id, y_val[train_id], alpha =0.5,color = 'green',label ='Observed past')
-    plt.plot(val_id, y_val[val_id],alpha =0.5,color ='red', label ='observed future')
-    plt.plot(val_id, preds[val_id], alpha =0.5, label ='Forecast')
-    plt.legend(loc ='upper left')
-    plt.title("Forecasting using LSTM model (encoder-decoder architecture) on " + DCs[0] 
-                + "(sliding window =" + str(time_window) + " ; n_layers=" + str(n_layers_autoencoder) + ")")
+    plt.plot(train_id, y_val[train_id], alpha=0.5,
+             color='green', label='Observed past')
+    plt.plot(val_id, y_val[val_id], alpha=0.5,
+             color='red', label='observed future')
+    plt.plot(val_id, preds[val_id], alpha=0.5, label='Forecast')
+    plt.legend(loc='upper left')
+    plt.title("Forecasting using LSTM model (encoder-decoder architecture) on " + DCs[0]
+              + "(sliding window =" + str(time_window) + " ; n_layers=" + str(n_layers_autoencoder) + ")")
+    plt.legend()
+    plt.savefig("plot.png", bbox_inches='tight', pad_inches=0.5, transparent=True)
+    plt.close()
